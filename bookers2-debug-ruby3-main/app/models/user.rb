6439fc4edf2e344e92bb7298aa3cc89cc78fc730
EC2 Default User <ec2-user@ip-172-31-43-29.eu-north-1.compute.internal>
2024-12-02 15:51:18 +0000

    120301

--- bookers2-debug-ruby3-main/app/models/user.rb
+++ bookers2-debug-ruby3-main/app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
          has_one_attached :profile_image
          validates :introduction, length:{ maximum: 50 }
          validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
-
+         has_many :favorites, dependent: :destroy
   
   
   def get_profile_image
