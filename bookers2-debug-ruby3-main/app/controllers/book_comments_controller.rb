e9a7663bda26b2da6623aa425d573ecc1c6f2a0d
EC2 Default User <ec2-user@ip-172-31-43-29.eu-north-1.compute.internal>
2024-12-08 16:36:04 +0000

    120901

--- bookers2-debug-ruby3-main/app/controllers/book_comments_controller.rb
+++ bookers2-debug-ruby3-main/app/controllers/book_comments_controller.rb
@@ -4,12 +4,16 @@ class BookCommentsController < ApplicationController
     comment = current_user.book_comments.new(book_comment_params)
     comment.book_id = book.id
     comment.save
-    redirect_to book_path(book)
+    @books=Book.find(params[:book_id])
+    @comment = BookComment.new
   end
 
   def destroy
     BookComment.find_by(id: params[:id], book_id: params[:book_id]).destroy
-    redirect_to request.referer
+    @books=Book.find(params[:book_id])
+    @book_comment=BookComment.find_by(id: params[:id], book_id: params[:book_id])
+    @book_comment.destroy
+    @book_comment = BookComment.new
   end
 
   private
